3a89a1a1bc78f4395111359ae051d1530a209558
IgorSFG <igor.garcia@sou.inteli.edu.br>
2023-04-29 22:29:47 -0300

    Aprender Assuntos

--- IPGodot/Contents.gd
+++ IPGodot/Contents.gd
@@ -1,16 +1,23 @@
 extends Node
-class_name Contents
 
 # Search
-const IPcontent = ["What is a IP.",
-"IP Types.",
-"Why IP is important.",
-"How identify a IP.",
+const IPcontent = ["What is a IP",
+"IP Types",
+"Why IP is important",
+"How identify a IP",
 "How protect a IP",
-"How comercialize a IP.",
-"IP violation."]
+"How comercialize a IP",
+"IP violation"]
 
 const DEVcontent = ["Book", "Art", "App", "Music", "Mark", "Patent"]
+
+const LRNcontent = ["Intellectual property refers to the rights that creators have over their creations and inventions. These rights allow them to have control and ownership of their intellectual works, such as writings, music, images, designs and technological innovations.",
+"Intellectual property can be such as copyrights, trademarks, patents, industrial designs, trade secrets and plant varieties. Copyright protects literary, artistic and musical works, while trademarks protect symbols, logos and names used in commerce. Patents, on the other hand, protect inventions and industrial designs protect the visual aspects of industrial products. Trade secrets protect confidential business information and plant varieties protect new and distinct varieties of plants.",
+"Intellectual property is important because it encourages creativity and innovation. It promotes economic, cultural and social development, preserving cultural diversity and intangible heritage and ensuring public safety and health. Copyright protection contributes to fair remuneration for creative work and encourages the production of new ideas.",
+"To identify intellectual property, you should start by determining what type of intellectual property you have, such as a trademark, patent, copyright, industrial design or trade secret. You can then conduct a search to see if someone else has already registered a similar intellectual property. It's also important to keep records of the creation and ownership of the intellectual property, including dates and any contracts or agreements related to it.",
+"Protecting intellectual property involves taking steps to prevent others from using, copying or distributing your creation without permission. Some ways to protect intellectual property include registering trademarks and patents, using copyright notices on works, using confidentiality agreements for trade secrets, and taking legal action against those who infringe your rights.",
+"Commercializing intellectual property can involve licensing, selling, or franchising your intellectual property. Respectively, involves granting permission, selling and allowing use under brand and business model. It's important to carefully consider the terms of any commercialization agreements and seek legal advice to ensure your rights are protected.",
+"Violations of intellectual property occur when someone uses, copies, or distributes another person's intellectual creation without proper authorization or payment. They can have serious consequences, including legal action, fines, and damages, and can negatively impact owners of intellectual property. It is crucial to respect and protect intellectual property to ensure continued innovation and creativity."]
 
 # Feelings:
 const introduce = "Let's develop something. I need gain some momey or I will break at the end of the month..."
